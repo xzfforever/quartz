b726dd8ed9d564f18b23219d72db6a7f7b09101e
xzf <270035679@qq.com>
2017-07-06 09:10:57 +0800

    project init

---
+++ distributed/quartz-distributed/src/main/java/com/xzf/spring/boot/quartzdistributed/QuartzDistributedApplication.java
@@ -0,0 +1,12 @@
+package com.xzf.spring.boot.quartzdistributed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzDistributedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzDistributedApplication.class, args);
+	}
+}
